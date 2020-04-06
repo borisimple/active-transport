694553883ea360c28b93dc5487eab2760c4e2bba
borisimple <borisimple@gmail.com>
2020-04-06 07:36:48 +0200

    added responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,18 @@ const vanContact = document.querySelector(".van-contact");
 const body = document.querySelector("body");
 const wrapper = document.querySelector(".wrapper");
 
+sectionContact.replaceChild(truckContact, vanContact);
+
 btnTruck.addEventListener("click", () => {
   wrapper.classList.replace("van", "truck");
   body.classList.replace("van", "truck");
+  sectionContact.classList.replace("van", "truck");
   sectionContact.replaceChild(truckContact, vanContact);
 });
 
 btnVan.addEventListener("click", () => {
   wrapper.classList.replace("truck", "van");
   body.classList.replace("truck", "van");
+  sectionContact.classList.replace("truck", "van");
   sectionContact.replaceChild(vanContact, truckContact);
 });
