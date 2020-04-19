d6de7613f0a8faf74aa53000963cdd777c709d0e
borisimple <borisimple@gmail.com>
2020-04-19 09:31:35 +0200

    temp

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const truckInfo = document.querySelector(".truck-info");
+const vanInfo = document.querySelector(".van-info");
 const copyright = document.querySelector("footer small");
 
 const date = new Date();
