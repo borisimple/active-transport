0a6a5d556e2d667587cf54b8283b59665868d9d1
borisimple <borisimple@gmail.com>
2020-04-20 18:58:38 +0200

    removed js

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-const truckInfo = document.querySelector(".truck-info");
-const vanInfo = document.querySelector(".van-info");
-const copyright = document.querySelector("footer small");
-
-const date = new Date();
-copyright.innerHTML = `Copyright &copy; ${date.getFullYear()} BORISK. All rights reserved.`;
