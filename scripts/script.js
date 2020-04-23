8caaa12a585c50afef342081c83965a9cf7ca0cb
borisimple <borisimple@gmail.com>
2020-04-23 07:50:21 +0200

    added loading animation

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-// const body = document.querySelector("body");
-// const img = document.querySelector("img");
-
-// img.addEventListener("mousemove", (e) => {
-//   img.style.marginLeft = -e.offsetY + "px";
-// });
